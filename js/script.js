310fd4d3dc777bb8c8497f251e80405d32d6fa0f
Rahul R S <rahulrsofficial@gmail.com>
2021-03-28 00:38:22 +0530

    2nd commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,19 @@ $(document).ready(function(){
       }else{
           $('.navbar').removeClass("sticky");
       }
+      if(this.scrollY > 200){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+        $('.scroll-up-btn').removeClass("show");
+      }
   });
+
+  //scroll up
+  $('.scroll-up-btn').click(function(){
+     $('html').animate({scrollTop: 0}); 
+     $('html').css("scrollBehavior", "auto");
+  });
+
 
   // toggle menu script
   $('.menu-btn').click(function(){
@@ -13,4 +25,34 @@ $(document).ready(function(){
       $('.menu-btn i').toggleClass("active");
   });
 
+  //typing animation
+  var typed = new Typed(".typing",{
+     strings: ["UI/UX designer","Artist"],
+     typeSpeed: 30,
+     backSpeed: 30,
+     loop:true,
+  });
+
+
+  //owl carousel script
+  $('.carousel').owlCarousel({
+       margin: 20,
+       loop: true,
+       autoplayTimeout : 2000,
+       autoplayHoverpause : true,
+       responsive: {
+           0:{
+               items: 1,
+               nav: false,
+           },
+           600:{
+            items: 2,
+            nav: false,
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+       }
+  });
 });
